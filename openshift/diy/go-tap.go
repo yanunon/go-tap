ff7f5de3c2b1b1540c5fd2c2a216cfd9d5bf10ae
Yang Junyong <yanunon@gmail.com>
2012-10-10 12:59:45 +0800

    edit README.md

--- openshift/diy/go-tap.go
+++ openshift/diy/go-tap.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	data_dir := os.Getenv("OPENSHIFT_DATA_DIR")
-	s := tap.NewServer("KEY", "SECRET", "your_id.rhcloud.com", "https", data_dir, 1)
+	s := tap.NewServer("KEY", "SECRET", "HOST", "https", data_dir, 1)
 	ip := os.Getenv("OPENSHIFT_INTERNAL_IP")
 	s.ListenAndServe(fmt.Sprintf("%s:8080", ip))
 }
